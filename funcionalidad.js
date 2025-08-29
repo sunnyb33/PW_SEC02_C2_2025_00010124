44265559919d1a4c381ac55b50a33d726409109e
Suni <00010124@uca.edu.sv>
2025-08-29 11:22:52 -0600

    :lipstick: :sparkles: Se añade mejor presentacion y funcionalidad de botones

--- funcionalidad.js
+++ funcionalidad.js
@@ -11,3 +11,36 @@ function change_title() {
     titulo.textContent = "University Demo";
   }
 }
+
+function change_h1Color() {
+  const secciones = document.querySelectorAll(".box h2");
+  secciones.forEach(element => { 
+    element.style.color = element.style.color === "red" ? "black" : "red";
+  });
+}
+
+function footer_image(){
+    document.getElementById('input-img').click();
+  }
+
+  document.getElementById("input-img").addEventListener("change", function(){
+    const footer = document.getElementById("footer");
+
+    if(this.files.length > 0){
+      const file = this.files[0];
+      const imgURL = URL.createObjectURL(file);
+
+      let oldImg = document.getElementById("footer-img");
+      if(oldImg) oldImg.remove();
+
+      const img = document.createElement("img");
+      img.id = "footer-img";
+      img.src = imgURL;
+      img.alt = "Imagen de pie de página";
+      img.style.width = "80px";
+      img.style.marginTop = "10px";
+      footer.appendChild(img);
+    }
+  });
+
+
